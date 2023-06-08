bad3462865bcc06c7837778a0e29a732971877bb
Chic0S87 <chicosepultura@gmail.com>
2023-06-08 13:05:46 -0300

    limpando cagadas

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import routerPessoa from "./Router/RouterPessoas.js";
 import rotaProduto from "./Router/rotaProduto.js"
-import rotaServices from "./Router/routerServices.js"
+import routerServico from "./Router/routerServicos.js";
 
 const server = express();
 server.use(cors({ origin: "*" }));
@@ -10,7 +10,7 @@ server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use("/pessoas", routerPessoa);
 server.use("/produto", rotaProduto);
-server.use("/service", rotaServices);
+server.use("/servicos", routerServico);
 server.listen(3308, "localhost", () => {
   console.log("Service running on http://localhost:3308 ");
 });
