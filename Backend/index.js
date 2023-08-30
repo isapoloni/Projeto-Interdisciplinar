08969d684b6ffc38a07991e72026b7bf9fb364b0
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-08-30 20:35:52 -0300

    jwt to verify and control access

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,17 @@ import rotaCategoriaProd from "./Router/rotaCategoriaProd.js";
 import cors from "cors";
 import rotaProduto from "./Router/rotaProduto.js";
 import routerServico from "./Router/routerServicos.js";
-
+import {verifyAccess, verifyJWT} from "./Router/verifyAccessAndControl.js";
 const server = express();
 server.use(cors({ origin: "*" }));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
-server.use("/pessoas", routerPessoa);
-server.use("/produto", rotaProduto);
-server.use("/servicos", routerServico);
-server.use("/categoria", rotaCategoriaProd);
+//Implementação do middleware de verificação JWT
+server.use("/pessoas", verifyJWT,routerPessoa);
+server.use("/produto", verifyJWT,rotaProduto);
+server.use("/servicos", verifyJWT,routerServico);
+server.use("/categoria",rotaCategoriaProd);
+server.use(verifyAccess);
 
 server.listen(3308, "localhost", () => {
   console.log("Service running on http://localhost:3308 ");
